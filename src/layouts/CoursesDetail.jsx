96c696792fe1cd76864d2b84dceca4e3ae280542
maigutter <maigutter@gmail.com>
2023-10-28 17:36:02 -0300

    more details and small minor changes in the layout pages

--- src/layouts/CoursesDetail.jsx
+++ src/layouts/CoursesDetail.jsx
@@ -1,33 +1,30 @@
 import ItemListContainer from "../Components/ItemListContainer";
 import PageTitle from "../Components/PageTitle";
 import { useParams } from "react-router-dom";
-import useArrayCoursesDetail from "../hooks/useArrayCoursesDetail";
+import { useContext } from "react";
 import CardShopDetail from "../Components/CardDetail";
+import useCourseById from "../hooks/useCourseById";
+import { Flex, Spinner } from "@chakra-ui/react";
+import CartContext from "../context/cart.context";
 
 function CoursesDetail() {
-  const { courseDetail } = useParams();
-  const { courseID, loading } = useArrayCoursesDetail(courseDetail);
+  const params = useParams();
+  const { course, isLoading } = useCourseById(params.courseId);
+  const { addItem } = useContext(CartContext);
 
-  if (loading) {
-    return <h4>Loading...</h4>;
+  if (isLoading) {
+    return <Spinner color="purple.600" />;
   }
 
-  if (!courseID) {
+  if (!course) {
     return <h3>Sorry! We can not find the course</h3>;
   }
   return (
-    <>
+    <Flex flexDir={"column"}>
       <PageTitle title="Detalle del Curso" />
       <ItemListContainer greeting="You can find the details of the course here" />
-      <CardShopDetail
-        key={courseID.id}
-        id={courseID.id}
-        Name={courseID.title}
-        Category={courseID.category}
-        Description={courseID.description}
-        Price={courseID.price}
-      />
-    </>
+      <CardShopDetail course={course} handleClick={addItem} />
+    </Flex>
   );
 }
 
--- src/layouts/CoursesShop.jsx
+++ src/layouts/CoursesShop.jsx
@@ -1,30 +1,26 @@
 import ItemListContainer from "../Components/ItemListContainer";
 import PageTitle from "../Components/PageTitle";
-import useArrayCourses from "../hooks/useArrayCourses";
-import CardShop from "../Components/Card";
+import { useContext } from "react";
+import CardShopDetail from "../Components/CardDetail";
+import useCourses from "../hooks/useCourses";
+import { Flex, Spinner } from "@chakra-ui/react";
+import CartContext from "../context/cart.context";
 
-const CoursesShop = () => {
-  {
-    const { courses } = useArrayCourses();
-    return (
-      <>
-        <PageTitle title="Cursos" />
-        <ItemListContainer greeting="You can find the different courses to buy here" />
-        <div className="displaySpace">
-          {courses.map((course) => (
-            <CardShop
-              key={course.id}
-              id={course.id}
-              Name={course.title}
-              Category={course.category}
-              Description={course.description}
-              Price={course.price}
-            />
-          ))}
-        </div>
-      </>
-    );
+function CoursesShop() {
+  const { course, isLoading } = useCourses();
+  const { addItem } = useContext(CartContext);
+
+  if (isLoading) {
+    return <Spinner color="purple.600" />;
   }
-};
+
+  return (
+    <Flex flexDir={"column"}>
+      <PageTitle title="Detalle del Curso" />
+      <ItemListContainer greeting="You can find the details of the course here" />
+      <CardShopDetail course={course} handleClick={addItem} />
+    </Flex>
+  );
+}
 
 export default CoursesShop;
